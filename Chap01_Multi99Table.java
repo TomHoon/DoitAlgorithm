0c17dcc5a87bce8f0c9e56079d9762caefb3ec3c
TomHoon <99778185+TomHoon@users.noreply.github.com>
2022-05-25 15:03:41 +0900

    Add files via upload

---
+++ Chap01_Multi99Table.java
@@ -0,0 +1,11 @@
+package Algorithm;
+//구구단
+public class Chap01_Multi99Table {
+	public static void main(String[] args){
+		for(int i=1; i<=9; i++){
+			for(int j=1; j<=9; j++){
+				System.out.println(i+"X"+j+"="+i*j);
+			}
+		}
+	}
+}
